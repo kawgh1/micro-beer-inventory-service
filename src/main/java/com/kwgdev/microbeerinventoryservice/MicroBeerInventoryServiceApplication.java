7e776053cacb62c58e8881cd1de38ef55f4d41d5
thrwwy216 <thrwwy216@tutanota.com>
2021-01-14 17:02:34 -0600

    init commit

---
+++ src/main/java/com/kwgdev/microbeerinventoryservice/MicroBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kwgdev.microbeerinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBeerInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroBeerInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwgdev/microbeerinventoryservice/MicroBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwgdev.microbeerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
